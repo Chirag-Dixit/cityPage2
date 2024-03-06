29b300f2adaad6a2c17f4abf394cc1959eac2d0a
chirag dixit <97054098+Chirag-Dixit@users.noreply.github.com>
2024-03-06 19:49:08 +0530

    [06/03/2024]: Added Weather App Functionalities

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,26 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloud } from "@fortawesome/free-solid-svg-icons";
 
 const Searchbar = (prop) => {
-  const {setSearch} = prop
-  const [searchValue, setSearchValue] = useState("")
-  const handleChange = (event)=>{
-    setSearchValue(()=>{
-      return event.target.value
-    })
-  }
+  const { setSearch } = prop;
+  const [searchValue, setSearchValue] = useState("");
+  const handleChange = (event) => {
+    setSearchValue(() => {
+      return event.target.value;
+    });
+  };
 
-  const handleSearch = ()=>{
-    setSearch((prev)=>{
-      return searchValue
-    })
-  }
+  const handleSearch = () => {
+    setSearch((prev) => {
+      return searchValue;
+    });
+  };
+
+  const clearSearch = () => {
+    setSearch((prev) => {
+      return "";
+    });
+  };
 
   return (
-    <div className='searchBar'>
-      <input onChange={handleChange} type='text' className='search'/>
-      <button onClick={handleSearch}>Search</button>
+    <div className="searchBar">
+      <FontAwesomeIcon
+        icon={faCloud}
+        beat
+        size="lg"
+        style={{ color: "#072a45", marginRight: "20px" }}
+      />
+      <input
+        onChange={handleChange}
+        type="text"
+        className="search"
+        placeholder="Enter City Name here..."
+        autoFocus
+      />
+      <button onClick={handleSearch} style={{ marginRight: "10px" }}>
+        Search
+      </button>
+      <button onClick={clearSearch}>Reset</button>
     </div>
-  )
-}
+  );
+};
 
-export default Searchbar
+export default Searchbar;
